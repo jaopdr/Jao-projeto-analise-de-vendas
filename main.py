0652d82d0cc444c8be62eaee1d64f1412b99e607
João Pedro <joaosezerino.dev@gmail.com>
2024-07-02 18:41:45 -0300

    def cont produtos

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from openpyxl import load_workbook
-from datetime import datetime
 
 excel = load_workbook('Relacao_Produtos_e_Clientes_2024.xlsx')
 
 planilha = excel.active
 
-def contar_produtos(file_path):
-    excel = load_workbook(file_path)
-    planilha = excel.active
-    
-    produtosA_contagem = {}
-    
+def contar_produtos():
+    produtosA_contagem = 0
+    produtosB_contagem = 0
+    produtosC_contagem = 0
+    produtosX_contagem = 0
+    produtosY_contagem = 0
+
     for row in planilha.iter_rows(min_row=2, min_col=2, max_col=2, values_only=True):
         produto = row[0]
-        
-        if produto not in produtosA_contagem:
-            produtosA_contagem[produto] = 0
-        
-        produtosA_contagem[produto] += 1
-    
-    return produtosA_contagem
+
+        if produto == "Produto A":
+            produtosA_contagem += 1
+        elif produto == "Produto B":
+            produtosB_contagem += 1
+        elif produto == "Produto C":
+            produtosC_contagem += 1
+        elif produto == "Produto X":
+            produtosX_contagem += 1
+        elif produto == "Produto Y":
+            produtosY_contagem += 1
+
+    return {
+        "Produto A": produtosA_contagem,
+        "Produto B": produtosB_contagem,
+        "Produto C": produtosC_contagem,
+        "Produto X": produtosX_contagem,
+        "Produto Y": produtosY_contagem
+    }
+
+contagem_produtos = contar_produtos()
+print(contagem_produtos)
